c5099d808894445e35a6bf0b837eae88efe96b4a
Kheayrig <kheayrig@gmail.com>
2022-11-18 00:04:04 +0300

    app context, download package for sqlite

--- MaternityHospital/DB/Models.cs
+++ MaternityHospital/DB/Models.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MaternityHospital.DB
 {
+    public class ApplicationContext : DbContext
     internal class Models
     {
+
     }
 }
