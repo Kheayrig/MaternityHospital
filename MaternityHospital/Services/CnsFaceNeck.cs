20d6ed90f8cb5ece8a7413141975eae308c5756a
Elizaveta <elizavetaviugina@mail.ru>
2022-12-11 11:44:22 -0800

    added classes for half of the windows

---
+++ MaternityHospital/Services/CnsFaceNeck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaternityHospital.Services
+{
+    class CnsFaceNeck
+    {
+        public string Lico { get; set; }
+        public string BokZhelydochki { get; set; }
+        public string PPPeregorodki { get; set; } = "норма";
+        public string TZhelydochek { get; set; }
+        public string CHZhelydochek { get; set; }
+        public string Talamys { get; set; } = "разделенный";
+        public string BCisterna { get; set; }
+
+    }
+}
---
+++ MaternityHospital/Services/Window2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaternityHospital.Services
+{
+    class Window2
+    {
+        public int  BR { get; set; }
+        public int DBK { get; set; }
+        public int OJ { get; set; }
+        public int  Mass { get; set; }
+
+    }
+}
