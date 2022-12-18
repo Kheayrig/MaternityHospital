77d3ff7d4f8459682faf435514c142b4de909319
Kheayrig <kheayrig@gmail.com>
2022-12-18 04:29:03 +0300

    add compare func for percentile corridors

--- MaternityHospital/DB/Models/PercentileCorridors.cs
+++ MaternityHospital/DB/Models/PercentileCorridors.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Configuration;
@@ -9,6 +10,14 @@ using System.Threading.Tasks;
 
 namespace MaternityHospital.DB.Models
 {
+    internal enum PercentileCorridorsField
+    {
+        Mass,
+        BR,
+        DB,
+        OZh,
+        DGK
+    }
     internal class PercentileCorridors
     {
         [Key]
@@ -44,21 +53,32 @@ namespace MaternityHospital.DB.Models
             MaxDGK = maxDGK;
         }
 
+        internal static int GetWeekBy(int value, PercentileCorridorsField field)
+        {
+            using(var db = new TemplateApplicationContext())
+            {
+                var str = db.TemplateValues
+                    .FromSqlRaw($"SELECT * FROM TemplateValues WHERE Min{field}<={value} AND Max{field}>={value}")
+                    .First();
+                return str.Week;
+            }
+        }
+
         internal static void AddDefault()
         {
-            string[] values;
-            var filePC = $"{Environment.CurrentDirectory}/data/{ConfigurationManager.AppSettings["PercentileCorridorsFileName"]}";
+            string[] data;
+            var filePC = $"{Environment.CurrentDirectory}/{ConfigurationManager.AppSettings["PercentileCorridorsFileName"]}";
             using (var sr = new StreamReader(filePC))
             {
-                while (!sr.EndOfStream)
-                {
-
-                }
-                values = sr.ReadLine().Split(',');
+                data = sr.ReadToEnd().Split(new char[] { '\n' , '\t'}, StringSplitOptions.RemoveEmptyEntries);
             }
             using (var db = new TemplateApplicationContext())
             {
-                db.TemplateValues.Add(new PercentileCorridors(12, 13, 25, 22, 24, 7, 11, 50, 71, 21, 25));
+                foreach (var item in data)
+                {
+                    var values = item.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+                    db.TemplateValues.Add(new PercentileCorridors(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], values[10]));
+                }
                 db.SaveChanges();
             }
         }
